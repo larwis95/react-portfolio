0e8222012cb743923b016f4a61580f5625efc710
Larry Wisniewski <larryw48390@gmail.com>
2024-06-23 02:35:15 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import HeroBody from "./components/Hero/herobody";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <HeroBody />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Hero/herobody.tsx
@@ -0,0 +1,10 @@
+export default function HeroBody(): JSX.Element {
+  return (
+    <div className="hero-body">
+      <div className="container">
+        <h1 className="title">Lawrence Wisniewski</h1>
+        <h2 className="subtitle">Full Stack Web Development</h2>
+      </div>
+    </div>
+  );
+}
