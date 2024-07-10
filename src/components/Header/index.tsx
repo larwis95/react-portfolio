51efc1284c63755ce17a1521e83051255cbb5fdd
Larry Wisniewski <larryw48390@gmail.com>
2024-07-10 03:47:41 -0400

    navigation component rename

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,10 @@
-import NavBar from "../Nav";
+import Navigation from "../Nav";
 import Hero from "../Hero/Hero";
 
 export default function Header(): JSX.Element {
   return (
     <>
-      <NavBar />
+      <Navigation />
       <Hero />
     </>
   );
